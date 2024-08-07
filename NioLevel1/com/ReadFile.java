1d39489990f53fc7795989cdb1a2021611d1f8d0
Dipanshu 1 <Dipanshu.1@godigit.com>
2024-08-07 17:52:34 +0530

    Read File

--- NioLevel1/com/ReadFile.java
+++ NioLevel1/com/ReadFile.java
@@ -1,15 +1,17 @@
 package NioLevel1.com;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class MergeMultipleToSingleFile {
+public class ReadFile {
     public static void main(String[] args) throws IOException {
         Path path=Paths.get("src/NioLevel1/com/loreum.txt");
        Stream<String> str=Files.lines(path);
         str.forEach(System.out::println);
+      //  Files.write(path,"HEyy ".getBytes(StandardCharsets.UTF_8));
     }
 }
